b99999a32f385771a717ea56187e1ed5fd4b43ee
FlorianBury <FlorianBury@github.com>
2020-01-14 16:22:44 +0100

    Added docstrings and cleared the tests

--- makeScaleFactorsDict.py
+++ makeScaleFactorsDict.py
@@ -6,10 +6,11 @@ import copy
 
 from pprint import pprint
 class MakeScaleFactorsDict():
-    def __init__(self,trigger_path,scalefactors_path):
-        self.trigger_path = trigger_path
-        self.scalefactors_path = scalefactors_path
-        self.full_dict = {}
+    def __init__(self,efficiencies_path,scalefactors_path,check_path=False):
+        self.efficiencies_path       = efficiencies_path
+        self.scalefactors_path  = scalefactors_path
+        self.check_path         = check_path 
+        self.full_dict          = {}
 
     def getScaleFactorsDict(self):
         return self.full_dict
@@ -23,13 +24,29 @@ class MakeScaleFactorsDict():
                 l.append(t)
         return tuple(l)
 
+    def makeFullEfficiencyPath(self,name):
+        full_path = os.path.join(self.efficiencies_path,name)
+        if os.path.isfile(full_path) or not self.check_path:
+            return full_path
+        else:
+            print ("[ERROR] Incorrect path to efficiency json file : %s"%full_path)
+
+    def makeFullScaleFactorPath(self,name):
+        full_path = os.path.join(self.scalefactors_path,name)
+        if os.path.isfile(full_path) or not self.check_path:
+            return full_path
+        else:
+            print ("[ERROR] Incorrect path to scalefactor json file : %s"%full_path)
+
+
 
     def addEfficiency(self,key_entry,base_str,format_dict):
         """
-        Add an efficiency entry to the dict -> {'key':tuple(jsonx4)}
-        base_str is a string containing {}(s) for formatting (with keys)
-        format_dict is a dict whose keys correspond to the ones in base_str and values are list of str to be formatted
+        key_entry   : new efficiency entry of the dict 
+        base_str    : string to be formatted with {keyword} (not empty, can be several)
+        format_dict : dict whose keys correspond to the ones in base_str and values are list of str to be formatted
         Note : if key already present, the content will be appended to the tuple
+        Note : if one doesn't want all combinations, the keys and related values can be expressed as tuples in format_dict
         """
         N_format = base_str.count('{')
         if len(format_dict.keys())>1: # More than one formatting
@@ -41,7 +58,7 @@ class MakeScaleFactorsDict():
                 # Flatten the nested comb if need be #
                 t = self.flattenTuple(c)
                 d = dict((k,v) for k,v in zip(keys,t))
-                aList.append(os.path.join(self.trigger_path,base_str.format(**d)))
+                aList.append(self.makeFullEfficiencyPath(base_str.format(**d)))
             aTup = tuple(aList)
         else:
             l = []
@@ -49,11 +66,11 @@ class MakeScaleFactorsDict():
                 if not isinstance(k,tuple):
                     for v in val:
                         d = {k:v}
-                        l.append(os.path.join(self.trigger_path,base_str.format(**d)))
+                        l.append(self.makeFullEfficiencyPath(base_str.format(**d)))
                 else:
                     for v in val:
                         d = dict((nk,nv) for nk,nv in zip(k,v))
-                        l.append(os.path.join(self.trigger_path,base_str.format(**d)))
+                        l.append(self.makeFullEfficiencyPath(base_str.format(**d)))
             aTup = tuple(l)
 
         # Check if key already present #
@@ -65,6 +82,14 @@ class MakeScaleFactorsDict():
             self.full_dict[key_entry] = aTup
 
     def addScaleFactor(self,key_entry,base_key,base_str,format_dict,sublevel=None,lowercase_keys=False):
+        """
+        key_entry   : new efficiency entry of the dict 
+        base_key    : string to be formatted with {keyword} (not empty, can be several) and used as nested dict key
+        base_str    : string to be formatted with {keyword} (not empty, can be several) and used as nested dict value
+        format_dict : dict whose keys correspond to the ones in base_str and base_key and values are list of str to be formatted
+        Note : if key already present, the content will be appended to the tuple
+        Note : if one doesn't want all combinations, the keys and related values can be expressed as tuples in format_dict
+        """
         N_format_key = base_key.count('{') 
         N_format_str = base_str.count('{')
         if len(format_dict.keys())>1: # More than one formatting
@@ -130,18 +155,15 @@ class MakeScaleFactorsDict():
                     comb_val = itertools.product(*[format_dict[k] for k in onlybase_str_formatkeys])
                 # Generate list of values comb and add that to tmp dict #
                 for combv in comb_val:
-                    print (combv)
                     nd = dict((k,v) for k,v in zip(onlybase_str_formatkeys,self.flattenTuple(combv)))
                     dv = {**dk,**nd}
-                    print (dv)
-                    print (os.path.join(self.scalefactors_path,base_str.format(**dv)))
                     if sublevel is not None: 
                         for key in dv.keys():# Find the associated key in the format_dict
                             if key in sublevel.keys(): 
                                 tup = sublevel[key][dv[key]] # Recover tuple corresponding to entry
-                                aList.append(tuple((tup,os.path.join(self.scalefactors_path,base_str.format(**dv)))))
+                                aList.append(tuple((tup,self.makeFullScaleFactorPath(base_str.format(**dv)))))
                     else:
-                        aList.append(os.path.join(self.scalefactors_path,base_str.format(**dv)))
+                        aList.append(self.makeFullScaleFactorPath(base_str.format(**dv)))
                         
                 aDict[base_key.format(**dk)] = tuple(aList)
         else:# Simple scalefactor
@@ -154,34 +176,13 @@ class MakeScaleFactorsDict():
                 else:
                     d = {key:v}
                 if lowercase_keys:
-                    aDict[base_key.format(**d).lower()] = os.path.join(self.scalefactors_path,base_str.format(**d))
+                    aDict[base_key.format(**d).lower()] = self.makeFullScaleFactorPath(base_str.format(**d))
                 else:
-                    aDict[base_key.format(**d)] = os.path.join(self.scalefactors_path,base_str.format(**d))
+                    aDict[base_key.format(**d)] = self.makeFullScaleFactorPath(base_str.format(**d))
 
         # Check if key already present #
         if key_entry in self.full_dict.keys():
             self.full_dict[key_entry].update(aDict)
         else:
             self.full_dict[key_entry] = aDict
-
-
-        
-
-inst = MakeScaleFactorsDict(os.path.join(os.path.abspath('.'),'data','TriggerEfficienciesStudies'),os.path.join(os.path.abspath('.'),'data','ScaleFactors_FullRunII'))
-#inst.addEfficiency('doubleEleLeg_HHMoriond17_2016','{wp}.json',{'wp':["Electron_IsoEle23Leg", "Electron_IsoEle12Leg", "Electron_IsoEle23Leg", "Electron_IsoEle12Leg"]})
-#inst.addEfficiency('doubleEleLeg_HHMoriond17_2016','{wp}_{calib}.json',{('wp','calib'):[("Electron_IsoEle23Leg",'testA'), ("Electron_IsoEle12Leg",'testB'), ("Electron_IsoEle23Leg",'testC'), ("Electron_IsoEle12Leg",'testA')]})
-#inst.addEfficiency('mutrig_2016_94X','{trig}_PtEtaBins_2016Run{eras}.json',{'trig':["IsoMu24_OR_IsoTkMu24","Mu50_OR_TkMu50"],'eras':["BtoF", "GtoH"]})
-#inst.addEfficiency('doubleEleLeg_HHMoriond17_2016','{type}_{other}_{wp}_{calib}.json',{'type':['TypeI','TypeII'],'other':['otherA','otherB'],('wp','calib'):[("Electron_IsoEle23Leg",'testA'), ("Electron_IsoEle12Leg",'testB'), ("Electron_IsoEle23Leg",'testC'), ("Electron_IsoEle12Leg",'testA')]})
-#inst.addScaleFactor('electron_2016_94X',"id_{wp}","Electron_EGamma_SF2D_2016Legacy_{wp}_Fall17V2.json",{"wp":["Loose", "Medium", "Tight", "MVA80","MVA90", "MVA80noiso", "MVA90noiso"]})
-#inst.addScaleFactor('btag_2016_94X','{algo}_{wp}',"BTagging_{wp}_{flav}_{algo}.json",{'algo':["DeepCSV", "DeepJet"],'wp':["loose", "medium", "tight"],'flav':["lightjets_incl","cjets_comb","bjets_comb"]})
-#inst.addScaleFactor('muon_2017_94X','id_{wp}',"Muon_NUM_{wp}ID_DEN_genTracks_pt_abseta_2017RunBCDEF.json",{'wp':["Loose", "Medium", "Tight", "Soft", "MediumPrompt"]},True)
-#inst.addScaleFactor('muon_2017_94X_supp','iso_{isowp}_id_{idwp}',"Muon_NUM_{isowp}RelIso_DEN_{idwp}ID_pt_abseta_2017RunBCDEF.json",{'wp':["Loose", "Medium", "Tight", "Soft", "MediumPrompt"]})
-inst.addScaleFactor('muon_2016_94X','id_{wp}',"Muon_NUM_{wp}ID_DEN_genTracks_eta_pt_{uncer}_2016Run{era}.json",
-                    {'wp':["Loose", "Medium", "Tight"],
-                     'uncer':['sys','stat'],
-                     'era':["BCDEF", "GH"]},
-                    sublevel = {'era':{'BCDEF':('Run2016B','Run2016C','Run2016D','Run2016E','Run2016F'),'GH':('Run2016G', 'Run2016H')}},
-                    lowercase_keys=True)
-dico = inst.getScaleFactorsDict()
-pprint (dico)
 
