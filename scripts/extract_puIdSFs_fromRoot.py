2ee2de5a4f892411ed41adfd5878de9f32c3acf4
FlorianBury <FlorianBury@github.com>
2020-11-23 16:34:47 +0100

    Updated the jet PU id SF script

--- scripts/extract_puIdSFs_fromRoot.py
+++ scripts/extract_puIdSFs_fromRoot.py
@@ -3,63 +3,90 @@
 import math
 import argparse
 import ROOT
+import json
+
+IGNORE_LAST_PT_BIN = False
 
 def format_eta_bin(eta_bin):
     return 'ptabseta<%.1f' % (eta_bin[1]) if (eta_bin[0] == 0) else 'ptabseta%.1f-%.1f' % (eta_bin[0], eta_bin[1])
 
-parser = argparse.ArgumentParser()
-parser.add_argument('file', help='ROOT file containing muons  scale factors')
-parser.add_argument('-s', '--suffix', help='Suffix to append at the end of the output filename', required=True)
-
-args = parser.parse_args()
-
-IGNORE_LAST_PT_BIN = False
-
-f = ROOT.TFile.Open(args.file)
-print (f)
-for key in f.GetListOfKeys():
-    wp = key.GetName()
-
-    h = f.Get(wp)
-
+def getBinning(h):
     eta_binning = []
-    for i in range(1, h.GetXaxis().GetNbins() + 1):
+    for i in range(1, h.GetYaxis().GetNbins() + 1):
         if len(eta_binning) == 0:
-            eta_binning.append(h.GetXaxis().GetBinLowEdge(i))
-            eta_binning.append(h.GetXaxis().GetBinUpEdge(i))
+            eta_binning.append(h.GetYaxis().GetBinLowEdge(i))
+            eta_binning.append(h.GetYaxis().GetBinUpEdge(i))
         else:
-            eta_binning.append(h.GetXaxis().GetBinUpEdge(i))
+            eta_binning.append(h.GetYaxis().GetBinUpEdge(i))
 
     pt_binning = []
-    for i in range(1, h.GetYaxis().GetNbins() + 1):
+    for i in range(1, h.GetXaxis().GetNbins() + 1):
         if len(pt_binning) == 0:
-            pt_binning.append(h.GetYaxis().GetBinLowEdge(i))
-            pt_binning.append(h.GetYaxis().GetBinUpEdge(i))
+            pt_binning.append(h.GetXaxis().GetBinLowEdge(i))
+            pt_binning.append(h.GetXaxis().GetBinUpEdge(i))
         else:
-            pt_binning.append(h.GetYaxis().GetBinUpEdge(i))
+            pt_binning.append(h.GetXaxis().GetBinUpEdge(i))
 
     if IGNORE_LAST_PT_BIN and len(pt_binning) > 2:
         pt_binning.pop()
 
-    eta = 'Eta' if eta_binning[0] < 0 else 'AbsEta'
-    json_content = {'dimension': 2, 'variables': [eta, 'Pt'], 'binning': {'x': eta_binning, 'y': pt_binning}, 'data': [], 'error_type': 'absolute'}
-    json_content_data = json_content['data']
+    return eta_binning,pt_binning
 
-    for i in range(0, len(eta_binning) - 1):
-        eta_data = {'bin': [eta_binning[i], eta_binning[i + 1]], 'values': []}
-        mean_eta = (eta_binning[i] + eta_binning[i + 1]) / 2.
-        for j in range(0, len(pt_binning) - 1):
-            mean_pt = (pt_binning[j] + pt_binning[j + 1]) / 2.
-            bin = h.FindBin(mean_eta, mean_pt)
-            error = h.GetBinError(bin)
-            pt_data = {'bin': [pt_binning[j], pt_binning[j + 1]], 'value': h.GetBinContent(bin), 'error_low': error, 'error_high': error}
+parser = argparse.ArgumentParser()
+parser.add_argument('--eff', required=True, help='ROOT file containing efficiency and mistag of the PU jet ID')
+parser.add_argument('--sf', required=True, help='ROOT file containing SF for efficiency and mistag + systematics')
 
-            eta_data['values'].append(pt_data)
+args = parser.parse_args()
 
-        json_content_data.append(eta_data)
 
-  
-    filename = 'puId_%s_%s.json' % (wp, args.suffix)
-    with open(filename, 'w') as j:
-        import json
-        json.dump(json_content, j, indent=2)
+
+for fargs,fName in zip([args.eff,args.sf],['EFF','SF']):
+    f = ROOT.TFile.Open(fargs)
+    keys = [key.GetName() for key in f.GetListOfKeys()]
+    for key in keys:
+        if 'Systuncty' in key:
+            continue
+
+        print (key)
+        h = f.Get(key)
+        h_syst = None
+        if key+'_Systuncty' in keys:
+            h_syst = f.Get(key+'_Systuncty')
+
+        eta_binning,pt_binning = getBinning(h)
+        if h_syst:
+            eta_binning_syst,pt_binning_syst = getBinning(h_syst)
+            assert eta_binning_syst == eta_binning
+            assert pt_binning_syst == pt_binning
+
+        eta = 'Eta' if eta_binning[0] < 0 else 'AbsEta'
+        json_content = {'dimension': 2, 'variables': [eta, 'Pt'], 'binning': {'x': eta_binning, 'y': pt_binning}, 'data': [], 'error_type': 'absolute'}
+        json_content_data = json_content['data']
+
+        for y in range(0, len(eta_binning) - 1):
+            eta_data = {'bin': [eta_binning[y], eta_binning[y + 1]], 'values': []}
+            mean_eta = (eta_binning[y] + eta_binning[y + 1]) / 2.
+            for x in range(0, len(pt_binning) - 1):
+                mean_pt = (pt_binning[x] + pt_binning[x + 1]) / 2.
+                bin = h.FindBin(mean_pt, mean_eta)
+                if h_syst:
+                    error = h_syst.GetBinContent(bin)
+                else:
+                    error = 0.
+                value = h.GetBinContent(bin)
+                if value == 0:
+                    print ('value 0',mean_eta,mean_pt)
+
+                pt_data = {'bin': [pt_binning[x], pt_binning[x + 1]], 'value': value, 'error_low': error, 'error_high': error}
+
+                eta_data['values'].append(pt_data)
+
+            json_content_data.append(eta_data)
+
+      
+        filename = 'PUID_%s_%s.json' % (fName,key)
+        print (filename)
+        with open(filename, 'w') as j:
+            json.dump(json_content, j, indent=2)
+    
+    f.Close()
