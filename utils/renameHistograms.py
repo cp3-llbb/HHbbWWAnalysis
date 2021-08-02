ed14311eaa0fa9b85ec3c77f82d0e81647ac024f
FlorianBury <FlorianBury@github.com>
2021-08-02 11:11:32 +0200

    Script to rename histograms

---
+++ utils/renameHistograms.py
@@ -0,0 +1,60 @@
+import os
+import sys
+import glob 
+import argparse
+import ROOT
+import enlighten
+import multiprocessing as mp
+
+from IPython import embed
+
+def rename_in_file(f,input,output,debug=False):
+    F = ROOT.TFile(f,"UPDATE")
+    histNames = [key.GetName() for key in F.GetListOfKeys() 
+                    if key.GetClassName().startswith('TH')]
+    count = 0
+    for histName in histNames:
+        if input not in histName:
+            continue
+        count += 1
+        if not debug:
+            outName = histName.replace(input,output)
+            h = F.Get(histName)
+            h.Write(outName,ROOT.TObject.kOverwrite)
+            F.Delete(histName+";1")
+    if count > 0:
+        print (f'Processing {f} : renamed {count}')
+    F.Close()
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Renaming root files histograms')
+    parser.add_argument('input', action='store',  type=str,
+                        help='Input string (to be replaced)')
+    parser.add_argument('output', action='store', type=str,
+                        help='Output string (used as replacement)')
+    parser.add_argument('regex', action='store',  type=str,
+                        help='Path to root files (can be a regex, then use quotes)')
+    parser.add_argument('--debug', action='store_true', required=False, default=False,
+                        help='Print without changing the names')
+    parser.add_argument('-j', dest='jobs',action='store', required=False, default=None, type=int,
+                        help='Number of processes to spawn')
+    args = parser.parse_args()
+
+
+    rootFiles = sorted(glob.glob(args.regex))
+    if len(rootFiles) == 0:
+        raise RuntimeError('Regex returned no file')
+    print (f'Looking at {len(rootFiles)} root files')
+    pbar = enlighten.Counter(total=len(rootFiles), desc='Progress', unit='files')
+    if args.jobs is None:
+        for rootFile in rootFiles:
+            rename_in_file(rootFile,args.input,args.output)
+            pbar.update()
+    else:
+        with mp.Pool(processes=args.jobs) as pool:
+            plotIt_configs = pool.starmap(rename_in_file,[(f,args.input,args.output,args.debug) 
+                                                        for f in rootFiles])
+
+
+
+
